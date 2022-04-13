63799552ab4ff80881520b5cfb66c38159dfca09
ernanijose <ernanijose@gmail.com>
2022-04-13 20:34:36 -0300

    Instalando as bibliotecas

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const PORT = 3001;
+server.listen(PORT, () =>{
+    console.log(`Servidor rodando na ${PORT}`);
+});
